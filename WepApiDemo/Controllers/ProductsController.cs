f282597af459831b54b6309a8565ffc0874ff3f3
Umur <umury@otokod.com>
2021-04-11 13:06:37 +0300

    Initial commit

---
+++ WepApiDemo/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WepApiDemo.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/[controller]/")]
+        public string Get()
+        {
+            return "Product1";
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/getUserList/")]
+        public string[] MyCon(int id)
+        {
+            return new string[] { "User1", "User2" };
+        }
+    }
+}
---
+++ WepApiDemo/Entities/Abstract/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WepApiDemo.Entities
+{
+    public interface IEntity<TEntity> where TEntity : class
+    {
+         IEnumerable<TEntity> GetAll();
+
+         void Delete(TEntity item);
+    }
+}
